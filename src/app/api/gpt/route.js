d36117607e7290d69fe6de4c6319d60d14f2814f
bin <oactua@outlook.com>
2025-03-18 09:03:15 +0800

    *)可手动写pre prompt

--- src/app/api/gpt/route.js
+++ src/app/api/gpt/route.js
@@ -23,7 +23,7 @@ export async function getOpenAIChatCompletion(prompt) {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const geminiModelName = process.env.GEMINI_MODEL_NAME || "gemini-1.5-flash"; // 默认使用 gemini-pro
 const geminiModel = genAI.getGenerativeModel({ model: geminiModelName }, { apiVersion: 'v1' });
-const fixedRoleInstruction = "你是一个AI，旨在回答和解决人们的问题，并且你使用与问题相同的语言回答问题。";
+const fixedRoleInstruction = process.env.GPT_PRE_PROMPT || "你是一个AI，旨在回答和解决人们的问题，并且你使用与问题相同的语言回答问题。";
 export async function getGeminiChatCompletion(prompt) {
   try {
     const combinedPrompt = `${fixedRoleInstruction}\n\n用户消息：${prompt}`;
