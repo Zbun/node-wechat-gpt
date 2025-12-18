62432c7aa85c0a55f05e97200749e117428a8d62
bin <oactua@outlook.com>
2025-12-18 13:38:45 +0800

    fix: simplify open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,17 +1,6 @@
 // open-next.config.ts
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
+const config = {
+  default: {},
 };
 
 export default config;
-
